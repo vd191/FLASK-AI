f27fe6e5f28d14cddd38d3a1d034eecfdae8d7a7
VD <30382979+vd191@users.noreply.github.com>
2024-10-06 00:37:16 +1000

    add random

--- features/whisper/whisper_controller.py
+++ features/whisper/whisper_controller.py
@@ -1,17 +1,41 @@
 from flask import Blueprint, request, jsonify
 from features.whisper import transcribe
+import random
 
 whisper_controller = Blueprint('whisper', __name__)
 
 @whisper_controller.route('/api/transcribe', methods=['POST'])
 def transcribe_audio():
+    print('transcribe_audio')
     try:
-        data = request.get_json()
-        audio = data['audio']
-        text = transcribe.audio_to_text(audio)
+        question = request.form['question']
+        question_id = request.form['questionId']
 
-        if (text):
-            return jsonify({"text": text}), 200
+        # Get the file from the request
+        if 'audio' not in request.files:
+            print("No audio file provided")
+            return "No audio file provided", 400
+        
+        audio = request.files['audio'] 
+
+        print(question)
+        print(question_id)
+        print(audio)
+
+        text = transcribe.audio_to_text(request)
+
+        score = {
+            'question': question,
+            'questionId': question_id,
+            'anwser': text,
+            'content': random.randint(1, 90),
+            'pronunciation': random.randint(1, 90),
+            'fluency': random.randint(1, 90),
+            'overall': random.randint(1, 90),
+        }
+        
+        if (score):
+            return jsonify(score), 200
         else:
             return jsonify({"status": "ERROR"}), 400 
     except:
